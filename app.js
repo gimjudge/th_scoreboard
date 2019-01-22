1b4b2fce056df4a98a8e7241313b322cae8ad37e
gimjudge <gimjudge@hotmail.com>
2019-01-22 14:46:37 -0800

    Props to Create Reusable Components

--- app.js
+++ app.js
@@ -7,23 +7,23 @@ const Header = (props) => {
 	);
 }
 
-const Player = () => {
+const Player = (props) => {
 	return (
 		<div className="player">
 			<span className="player-name">
-				Dan
+				{ props.name }
 			</span>
 
-			<Counter />
+			<Counter score={ props.score }/>
 		</div>
 	);
 }
 
-const Counter = () => {
+const Counter = (props) => {
 	return (
 		<div className="counter">
 			<button className="counter-action decrement"> - </button>
-			<span className="counter-score">32</span>
+			<span className="counter-score">{ props.score }</span>
 			<button className="counter-action increment"> + </button>
 		</div>
 	);
@@ -32,9 +32,16 @@ const Counter = () => {
 const App = () => {
 	return (
 		<div className="scoreboard">
-			<Header title="Scoreboard" totalPlayers={2}/>
+			<Header 
+				title="Scoreboard" 
+				totalPlayers={4}
+			/>
+
 			{/* Player List */}
-			<Player />
+			<Player name = "Daniel" score = {35} />
+			<Player name = "Guil" score = {40} />
+			<Player name = "Dan" score = {45} />
+			<Player name = "Dani" score = {3} />
 		</div>
 	);
 }
